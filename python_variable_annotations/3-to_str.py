431ca0ecca882464c71ba89481d5143ad76ee8f2
christophemr <christophemr59@gmail.com>
2024-04-22 09:25:35 +0200

    indentation plus whitespace fix

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -5,6 +5,6 @@ argument and returns the string representation of the float
 """
 
 
-def to_str(n: float) ->str:
-  """returns the string representation of the float"""
-  return str(n)
+def to_str(n: float) -> str:
+    """returns the string representation of the float"""
+    return str(n)
